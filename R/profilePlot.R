4d882d4c8ff2853e73a5a53fa9713da623439ae3
Willem Talloen <wtalloen@its.jnj.com>
2012-03-22 08:49:03 +0000

    extra colors in a4palette
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/a4Base@64267 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/profilePlot.R
+++ R/profilePlot.R
@@ -1,12 +1,78 @@
-a4palette <- function(n){
-  if (!is.numeric(n) | n < 1)
-    stop("'n' should be a positive integer")
-  res <- if (n==1) "red"
-      else if (n==2) c("red","blue")
-      else if (n==3) c("red","green","blue")
-      else if (n==4) c("red","green","blue","purple")
-      else rainbow(n)
-  return(res)
+#' Pick One or More OA Colors
+#' @param color a character vector of color names; possible values are "red", "orange", "yellow", "green", "cyan", "blue", "pink", 
+#'       "limegreen", "purple", "black", "white", "grey" or "gray" 
+#' @param alpha transparency level for the color(s)
+#' @return character vector of colors 
+#' @author Tobias Verbeke
+#' @export
+oaColors <- function(color = NULL, alpha = 1.0){
+	
+	colPalette1 <- hcl(seq(0, 360, by = 10 ), l = 50, c = 250, alpha = alpha)
+	colPalette2 <- hcl(seq(250, 270, by = 1), l = 50, c = 360, alpha = alpha)
+	
+	colorValues <- c(colPalette1[2], colPalette2[6], colPalette2[15], colPalette1[12], 
+			colPalette1[21], colPalette1[27], colPalette1[29], 
+			rainbow(60, alpha = alpha)[17], rainbow(360, alpha = alpha)[278], 
+			hcl(180, c = 0, l = 0, alpha = alpha), hcl(269, l = 100, c = 90, alpha = alpha), 
+			hcl(180, l = 60, c = 0, alpha = alpha), hcl(180, l = 60, c = 0, alpha = alpha)
+	)
+	names(colorValues) <- c("red", "orange", "yellow", "green", "cyan", "blue", "pink", 
+			"limegreen", "purple", "black", "white", "grey", "gray")
+	
+	out <- colorValues[color]
+	
+	if(any(is.na(out)))
+		stop(
+				cat("Error: ", color[which(is.na(out))], " is not in the list of oaColors. Current colors include: ", "\n         ", 
+						paste(paste(names(colorValues), collapse = ", "), sep = ""), "\n", sep = "")
+		)
+	
+	
+	return(out)
+	
+}
+
+
+
+#' Generate a Palette of OA Colors
+#' @param numColors number of colors to be contained in the palette
+#' @param alpha transparency level of the colors
+#' @return vector of colors
+#' @author Jason Waddell
+#' @export
+oaPalette <- function(numColors = NULL, alpha = 1.0){
+	
+	# check numColors = NULL, test range
+	
+	fullPaletteNames <- c("red", "blue", "green", "orange", "pink", "cyan", "yellow", "limegreen", "purple")
+	
+	samplePaletteNames <- fullPaletteNames[1:numColors]
+	samplePalette <- oaColors(samplePaletteNames, alpha = alpha)
+	return(samplePalette)
+	
+}
+
+a4palette <- function(n, alpha = 1.0, Janssen = FALSE){
+	if (!is.numeric(n) | n < 1)
+		stop("'n' should be a positive integer")
+	
+	if(Janssen){
+		# Janssen ppt template colors
+		col1 <- rgb(9,53,122, alpha = alpha, max=255)
+		col2 <- rgb(42,142,191, alpha = alpha, max=255)
+		col3 <- rgb(148,198,223, alpha = alpha, max=255)
+		col4 <- rgb(35,125,38, alpha = alpha, max=255)
+		col5 <- rgb(127,195,28, alpha = alpha, max=255)
+		col6 <- rgb(191,225,141, alpha = alpha, max=255)
+		
+		res <- if (n==1) col2
+				else if (n==2) c(col2, col4)
+				else if (n==3) c(col1, col2, col4)
+				else if (n==4) c(col1, col2, col4, col5)
+				else rainbow(n)
+		return(res)
+		
+	} else {if (n < 10) oaPalette(n, alpha)	else if (n==2) rainbow(n)}
 }
 
 ### intensity plot
